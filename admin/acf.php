66cbce53336e9132797eeb80ed7933df2944d920
Kazue Igarashi <igarashi@digitalcube.jp>
2019-11-18 17:26:30 +0900

    add speaker_type acf

--- admin/acf.php
+++ admin/acf.php
@@ -1240,3 +1240,71 @@ acf_add_local_field_group(array(
 	'active' => true,
 	'description' => '',
 ));
+
+// スピーカー種別
+acf_add_local_field_group(array(
+	'key' => 'group_5dd24349de063',
+	'title' => '賞',
+	'fields' => array(
+		array(
+			'key' => 'field_5dd248d26c92f',
+			'label' => 'ラベル',
+			'name' => 'award_label',
+			'type' => 'text',
+			'instructions' => 'スピーカーがゲームチェンジャーの子カテゴリーに属する場合のみ出力されます',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => '',
+		),
+		array(
+			'key' => 'field_5dd249006c930',
+			'label' => '画像',
+			'name' => 'award_ image',
+			'type' => 'image',
+			'instructions' => 'スピーカーがゲームチェンジャーの子カテゴリーに属する場合のみ出力されます',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'id',
+			'preview_size' => 'medium',
+			'library' => 'all',
+			'min_width' => '',
+			'min_height' => '',
+			'min_size' => '',
+			'max_width' => '',
+			'max_height' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'taxonomy',
+				'operator' => '==',
+				'value' => 'speaker_type',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
